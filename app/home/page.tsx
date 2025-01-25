f51ff7b1859692e92ae03f78c4a7e9e2b57c61b2
Rugsit <rugsit.nest@gmail.com>
2025-01-25 20:35:21 +0700

    Edit something

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -4,7 +4,7 @@ import { Timer } from "@/components/timer";
 export default function Home() {
   return (
     <section className='bg-gradient-to-b from-white to-primary-500 '>
-      <div className="h-[100vh] flex items-center justify-center">
+      <div className="h-[100vh]  flex items-center justify-center pt-[100px]">
         <Timer title="ปิดลงวันเวลาสัมภาษณ์ในอีก..." endTime="1234" buttonAction="ลงเวลาสัมภาษณ์" desc="คุณยังไม่เลือกวันเวลาสัมภาษณ์โปรดเลือกเวลาภายในช่วงเวลาที่กำหนด"/>
       </div>
       <HomeDetails />
