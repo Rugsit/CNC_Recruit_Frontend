6f4d010f7dbbfe5a50cd07e17939b4c0cc9f644c
Tanaroeg O-Charoen <tanaroeg.o@ku.th>
2025-02-18 23:49:03 +0700

    add login debug

--- auth.ts
+++ auth.ts
@@ -34,8 +34,8 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
           });
           const data = await res.json();
 
-          // console.log( "responses status : ", res.status)
-          // console.log('data : ', data);
+          console.log( "responses status : ", res.status)
+          console.log('data : ', data);
           token.backendToken = data.token;
           token.role = data.role;
           token.id = data.id;
